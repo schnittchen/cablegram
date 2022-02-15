df0343812b9243d6aee7ea844f99aea1e77c83a2
Thomas Stratmann <thomas.stratmann@9elements.com>
2022-02-15 22:03:32 +0100

    remove most of module stub

--- lib/cablegram.ex
+++ lib/cablegram.ex
@@ -2,17 +2,4 @@ defmodule Cablegram do
   @moduledoc """
   Documentation for `Cablegram`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cablegram.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
