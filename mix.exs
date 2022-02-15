1bf89b9204f7a3da1b9280535d45de667fbcf32b
Thomas Stratmann <thomas.stratmann@9elements.com>
2022-02-15 23:05:29 +0100

    configure ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Cablegram.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Cablegram",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
